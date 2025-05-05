671e466fbcd8b5eb77884a935630d3ad79e8cf70
Ajit Verma <143831302+ajitverma07@users.noreply.github.com>
2025-05-05 15:44:01 +0530

    Update history.jsx

--- pages/history.jsx
+++ pages/history.jsx
@@ -50,28 +50,28 @@ const Contact = () => {
         </div>
       </div>
       <p className={styles.desc}>
-        The Sahrawi tea, known locally and nationally as "Atay," is prepared in
-        a special way according to a very distinctive ritual. It is essential to
-        have the "Atay utensils," which are various necessary vessels
-        specifically designed for it, before anything else. These utensils
-        include a clay or metal container called "Al-Majmar," where the embers
-        are placed to allow the positioning of the "Al-Maghrej" or teapot. This
-        is a special container in which water boils to prepare the tea.
-        Additionally, on it, later on, the tea pot, known as "Al-Mijmar," is
-        placed to simmer over a low flame. This is done without forgetting the
-        "Arabous," a tool used to blow into the fire to kindle it. Furthermore,
-        the "Sinya" or tray is indispensable, which is a flat, round vessel
-        usually made of silver or copper. The tea is served in it using the tea
-        pot or pitcher, with a famous type called "Tasmimat." Around it are
-        small glass cups in which tea is presented to guests. The tray may also
-        include a larger metal cup, designated for the quantity of water in
-        which the tea leaves have been soaked, called "Kass Atchellila." In
-        addition to the "Rabayat," which are two small containers: the first,
-        called "Al-Zanbil," is dedicated to tea, and the second, "Al-Rbi'a," is
-        designated for sugar. There is also the "Al-Makassara," which is the
-        tool used to crush sugar into particles of various sizes. Not to forget
-        the "Zeif Al-Keesan," meaning the napkin used to dry the cups, ensuring
-        their cleanliness and shining brightness.
+       The Sahrawi tea, known locally and nationally as &quot;Atay,&quot; is prepared in
+  a special way according to a very distinctive ritual. It is essential to
+  have the &quot;Atay utensils,&quot; which are various necessary vessels
+  specifically designed for it, before anything else. These utensils
+  include a clay or metal container called &quot;Al-Majmar,&quot; where the embers
+  are placed to allow the positioning of the &quot;Al-Maghrej&quot; or teapot.
+  This is a special container in which water boils to prepare the tea.
+  Additionally, on it, later on, the tea pot, known as &quot;Al-Mijmar,&quot; is
+  placed to simmer over a low flame. This is done without forgetting the
+  &quot;Arabous,&quot; a tool used to blow into the fire to kindle it. Furthermore,
+  the &quot;Sinya&quot; or tray is indispensable, which is a flat, round vessel
+  usually made of silver or copper. The tea is served in it using the tea
+  pot or pitcher, with a famous type called &quot;Tasmimat.&quot; Around it are
+  small glass cups in which tea is presented to guests. The tray may also
+  include a larger metal cup, designated for the quantity of water in
+  which the tea leaves have been soaked, called &quot;Kass Atchellila.&quot; In
+  addition to the &quot;Rabayat,&quot; which are two small containers: the first,
+  called &quot;Al-Zanbil,&quot; is dedicated to tea, and the second, &quot;Al-Rbi&apos;a,&quot; is
+  designated for sugar. There is also the &quot;Al-Makassara,&quot; which is the
+  tool used to crush sugar into particles of various sizes. Not to forget
+  the &quot;Zeif Al-Keesan,&quot; meaning the napkin used to dry the cups, ensuring
+  their cleanliness and shining brightness.
       </p>
     </div>
   );
